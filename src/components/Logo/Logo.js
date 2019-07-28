f1372a8180b728ec95cd7ab42ce1702714919a7f
Shirley940408 <Shirley940408@gmail.com>
2019-07-28 17:35:10 -0400

    using the samename in scss

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,7 +4,7 @@ import classes from './Logo.module.scss';
 
 const Logo = props => (
   <div className = {classes.Logo}>
-    <img src= {burgerLogo} alt="myBurger"/>
+    <img src= {burgerLogo} alt="myBurger" />
   </div>
 )
 
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -6,7 +6,9 @@ import NavigationItems from '../NavigationItems/NationItems';
 const Toolbar = props  => (
   <header className = {classes.Toolbar}>
     <div>MENU</div> 
-    <Logo />
+    <div className = {classes.Logo}>
+      <Logo />
+    </div>
     <nav>
       <NavigationItems />
     </nav>
