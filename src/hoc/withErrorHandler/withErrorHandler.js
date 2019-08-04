86afcc8cd75ca31729cc4261683fbec05eb7e5d5
Shirley940408 <Shirley940408@gmail.com>
2019-08-04 00:13:04 -0400

    add eject intercepter to release the RAM rooms for unused instance of axios

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -3,25 +3,47 @@ import Modal from '../../components/UI/Modal/Modal';
 
 const withErrorHandler = (WrappedComponent, axios) => {
   return class extends Component {
+    state = {
+      error: null,
+    }
     constructor(props){
       super(props);
-      axios.interceptors.request.use(req => {
+      this.reqInterceptor = axios.interceptors.request.use(req => {
         this.setState({error: null});
         return req;
       })
-      axios.interceptors.response.use(res => res, error => {
+      this.resInterceptor = axios.interceptors.response.use(res => res, error => {
         /* res => res means just ruturn the response, 
         it is another form of 'return response;' */
         this.setState({error: {message: 'Network Error'}});
       })
     }
-    state = {
-      error: null
+/**
+ * why I did not use axios interceptors in component instead using it in constructor? 
+ * It is because componentDidMount would only work after all childern render, 
+ * thus, it will not show when the children has the error when sending the requests.
+ * The interceptor should be settled in componentWillMount() lifeCycle for render it before its children. 
+ * However, considering the componentWillMount() lifeCycle will be canceled by React in the future, 
+ * moving it to the constructor is the best solution.
+ */
+    // componentDidMount () {
+      // axios.interceptors.request.use(req => {
+      //   this.setState({error: null});
+      //   return req;
+      // })
+      // axios.interceptors.response.use(res => res, error => {
+      //   /* res => res means just ruturn the response, 
+      //   it is another form of 'return response;' */
+      //   this.setState({error: {message: 'Network Error'}});
+      // })
+    // }
+
+    componentWillUnmount () {
+      console.log('will Unmount', this.reqInterceptor, this.resInterceptor);
+      axios.interceptors.request.eject(this.reqInterceptor);
+      axios.interceptors.response.eject(this.resInterceptor);
     }
 
-    // componentDidMount () {
-
-    // }
     errorConfirmedHandler = () => {
       this.setState({error: null})
     }
